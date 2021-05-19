5a8d6566a92e7727e541c8d881156d0d69c9b3a3
alexey_horbunov <algoritm211@gmail.com>
2021-05-20 00:00:35 +0300

    Solve syntax mistake in cloudinary.config.js

--- src/cloudinary/cloudinary.config.js
+++ src/cloudinary/cloudinary.config.js
@@ -2,7 +2,7 @@ const cloudinary = require('cloudinary')
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDUNARY_API_KEY,
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 })
 
