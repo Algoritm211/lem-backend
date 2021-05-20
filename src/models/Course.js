a434962e5f0e908879fd9059fc2112c282e07379
alexey_horbunov <algoritm211@gmail.com>
2021-05-21 00:07:01 +0300

    Add subject and about fields in Course model

--- src/models/Course.js
+++ src/models/Course.js
@@ -1,10 +1,15 @@
 const { model, Schema, ObjectId } = require('mongoose')
 
+// All types of subjects:
+// const subjectTypes = ['math', 'it', 'chemistry', 'physics', 'biology', 'history']
+
 const Course = new Schema({
   title: { type: String },
   description: { type: String },
+  about: { type: String },
   category: { type: String },
   rating: { type: Number },
+  subject: { type: String },
   coursePreview: { type: String, default: '' },
   isReady: { type: Boolean },
   author: { type: ObjectId, ref: 'User' },
