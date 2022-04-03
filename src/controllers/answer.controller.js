5a72813b06c2adf15a5d24d38f3d94da20ca4995
ek <ek@blaster.ai>
2022-04-03 04:34:29 +0300

    Removed initial columnsData, we'll pass init columns on front, 'cause of sorting

--- src/controllers/answer.controller.js
+++ src/controllers/answer.controller.js
@@ -1,138 +1,124 @@
-const Answer = require('../models/Answer')
-const Course = require('../models/Course')
-const consola = require('consola')
+const Answer = require("../models/Answer");
+const Course = require("../models/Course");
+const consola = require("consola");
 
 class AnswerController {
   async addNewAnswer(req, res) {
     try {
-      const { text, score, stepType, stepId: step, userId: user } = req.body
+      const { text, score, stepType, stepId: step, userId: user } = req.body;
 
       const newAnswer = new Answer({
-        text: text || '',
+        text: text || "",
         score: score || 0,
         step,
         stepType,
         user,
-      })
+      });
 
-      await newAnswer.save()
+      await newAnswer.save();
       return res.status(201).json({
         answer: newAnswer,
-        message: 'Answer created successfully',
-      })
+        message: "Answer created successfully",
+      });
     } catch (error) {
-      consola.error(error)
-      return res.status(500).json({ message: 'Error while adding answer' })
+      consola.error(error);
+      return res.status(500).json({ message: "Error while adding answer" });
     }
   }
 
   async getAnswersByCourse(req, res) {
     try {
-      const { courseId } = req.params
+      const { courseId } = req.params;
 
       const coursesWithSteps = await Course.findOne({
         _id: courseId,
       })
-        .populate('students', ['name', 'surName'])
+        .populate("students", ["name", "surName"])
         .populate({
-          path: 'lessons',
+          path: "lessons",
           populate: {
-            path: 'steps',
+            path: "steps",
           },
-        }).select('lessons')
+        })
+        .select("lessons");
 
-      const allCourseStepIds = coursesWithSteps
-        .lessons
+      const allCourseStepIds = coursesWithSteps.lessons
         .flatMap((lesson) => lesson.steps)
-        .map((step) => step.stepId)
+        .map((step) => step.stepId);
 
       const answers = await Answer.find({ step: { $in: allCourseStepIds } })
         .populate({
-          path: 'step',
+          path: "step",
           populate: {
-            path: 'lesson',
-            select: ['title', 'course'],
+            path: "lesson",
+            select: ["title", "course"],
             populate: {
-              path: 'course',
-              select: ['title'],
+              path: "course",
+              select: ["title"],
             },
           },
         })
         .populate({
-          path: 'user',
-          select: ['name', 'surName'],
-        })
+          path: "user",
+          select: ["name", "surName"],
+        });
 
       // Transforming users and their marks
-      const userWithAnswers = {}
+      const userWithAnswers = {};
 
       answers.forEach((answer, index) => {
         if (userWithAnswers.hasOwnProperty(answer.user._id)) {
-          userWithAnswers[answer.user._id][answer.step._id] = answer.score
-          userWithAnswers[answer.user._id].totalScore += answer.score
+          userWithAnswers[answer.user._id][answer.step._id] = answer.score;
+          userWithAnswers[answer.user._id].totalScore += answer.score;
         } else {
           userWithAnswers[answer.user._id] = {
             name: `${answer.user.name} ${answer.user.surName}`,
             userId: answer.user._id,
             totalScore: answer.score,
             [answer.step._id]: answer.score,
-          }
+          };
         }
-      })
+      });
 
-      const tableData = Object.values(userWithAnswers)
+      const tableData = Object.values(userWithAnswers);
 
       // Transforming courses
-      const columnsData = [
-        {
-          title: 'UserId',
-          dataIndex: 'userId',
-          key: 'userId',
-        },
-        {
-          title: 'Name',
-          dataIndex: 'name',
-          key: 'name',
-        },
-        {
-          title: 'Score',
-          dataIndex: 'totalScore',
-          key: 'totalScore',
-        },
-      ]
+      const columnsData = [];
 
-      const coursesObj = {}
+      const coursesObj = {};
 
       answers.forEach((answer) => {
         if (coursesObj.hasOwnProperty(answer.step.lesson._id)) {
           coursesObj[answer.step.lesson._id].children.push({
             title: `Q${coursesObj[answer.step.lesson._id].children.length + 1}`,
             dataIndex: answer.step._id,
-          })
+          });
         } else {
           coursesObj[answer.step.lesson._id] = {
             title: answer.step.lesson.title,
-            children: [{
-              title: 'Q1',
-              dataIndex: answer.step._id,
-            }],
-          }
+            children: [
+              {
+                title: "Q1",
+                dataIndex: answer.step._id,
+              },
+            ],
+          };
         }
-      })
+      });
 
-
-      columnsData.push(...Object.values(coursesObj))
+      columnsData.push(...Object.values(coursesObj));
 
       return res.status(200).json({
         columnsData,
         tableData,
-      })
+      });
     } catch (error) {
-      consola.error(error)
-      return res.status(500).json({ message: 'Error getting answers by course' })
+      consola.error(error);
+      return res
+        .status(500)
+        .json({ message: "Error getting answers by course" });
     }
   }
 }
 
-
-module.exports = new AnswerController()
+module.exports = new AnswerController();
